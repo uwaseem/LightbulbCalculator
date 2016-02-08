503e8f4d82473344be922011a61036cc6c1ee65f
uwaseem <ubaidillah@iflix.com>
2016-02-08 20:24:08 +0800

    Calculate bulbs

--- assets/calculator.js
+++ assets/calculator.js
@@ -3,12 +3,26 @@
   var app = angular.module('myCalculator', []);
 
   app.controller('CalculatorController', ['$scope', function($scope){
-    console.log('Heyya Mother Fuckah !!');
-    $scope.lumenOptions = [375, 600, 900, 1125, 1600];
+    $scope.LUMEN_OPTIONS = [375, 600, 900, 1125, 1600];
     $scope.currentLumens = 600;
     $scope.currentCost = 12;
     $scope.currentHours = 3;
-    $scope.totalDays = 365;
+    $scope.TOTAL_DAYS = 365;
+
+    $scope.INC_CONVERSION = .0625;
+    $scope.HAL_CONVERSION = .0450;
+    $scope.CFL_CONVERSION = .0146;
+    $scope.LED_CONVERSION = .0125;
+
+    $scope.calculate = function() {
+      console.log('calcualting');
+      $scope.incWattage = ($scope.currentLumens * $scope.INC_CONVERSION).toFixed(2);
+      $scope.halWattage = ($scope.currentLumens * $scope.HAL_CONVERSION).toFixed(2);
+      $scope.cflWattage = ($scope.currentLumens * $scope.CFL_CONVERSION).toFixed(2);
+      $scope.ledWattage = ($scope.currentLumens * $scope.LED_CONVERSION).toFixed(2);
+    }
+
+    $scope.calculate();
   }]);
 })();
 

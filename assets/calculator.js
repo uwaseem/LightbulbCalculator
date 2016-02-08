242288c4c04ea5c9204da4414e00b9ef0e8050c9
uwaseem <ubaidillah@iflix.com>
2016-02-08 20:37:56 +0800

    Update calculations for cost

--- assets/calculator.js
+++ assets/calculator.js
@@ -15,11 +15,20 @@
     $scope.LED_CONVERSION = .0125;
 
     $scope.calculate = function() {
-      console.log('calcualting');
+      $scope.currentHours = ($scope.currentHours > 24) ? 24 : $scope.currentHours;
+
+      var totalHours = $scope.currentHours * $scope.TOTAL_DAYS;
+      var cost = $scope.currentCost/100;
+
       $scope.incWattage = ($scope.currentLumens * $scope.INC_CONVERSION).toFixed(2);
       $scope.halWattage = ($scope.currentLumens * $scope.HAL_CONVERSION).toFixed(2);
       $scope.cflWattage = ($scope.currentLumens * $scope.CFL_CONVERSION).toFixed(2);
       $scope.ledWattage = ($scope.currentLumens * $scope.LED_CONVERSION).toFixed(2);
+
+      $scope.incCost = ((($scope.incWattage * totalHours)/1000) * cost).toFixed(2);
+      $scope.halCost = ((($scope.halWattage * totalHours)/1000) * cost).toFixed(2);
+      $scope.cflCost = ((($scope.cflWattage * totalHours)/1000) * cost).toFixed(2);
+      $scope.ledCost = ((($scope.ledWattage * totalHours)/1000) * cost).toFixed(2);
     }
 
     $scope.calculate();
